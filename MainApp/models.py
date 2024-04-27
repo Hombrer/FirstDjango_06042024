3c03268d9f24a7796d6853aa22c144da8f4394b3
Vadim <5208643@mail.ru>
2024-04-27 13:00:47 +0300

    Add Color model

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self) -> str:
+        return f"Color({self.name})"
+
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
     description = models.TextField(max_length=200, default="Базовое описание товара")
+    colors = models.ManyToManyField(to=Color)
 
     def __str__(self) -> str:
         return f"Item({self.name})"
